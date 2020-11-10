f0dd5a38ed7ee89c49efc1a061dc0eaf12b7ab5a
Alain Rusnov <alainrusnov@gmail.com>
2020-11-10 14:33:26 -0500

    added create, update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,25 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
